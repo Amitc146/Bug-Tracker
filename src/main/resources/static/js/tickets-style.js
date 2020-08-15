a12dccbceec089fc0b6904376a6c693dd86b0121
Amit <amitc146@gmail.com>
2020-08-15 14:16:44 +0300

    Forms fixes

--- src/main/resources/static/js/tickets-style.js
+++ src/main/resources/static/js/tickets-style.js
@@ -1,18 +1,9 @@
 const tickets = document.querySelectorAll(".tickets-list");
 
-setTicketsStyles(tickets);
+for (let ticket of tickets) {
+    let priority = ticket.querySelector(".ticket-priority");
 
-function setTicketsStyles(tickets) {
-    for (let ticket of tickets) {
-        let priority = ticket.querySelector(".ticket-priority");
-        let status = ticket.querySelector(".ticket-status");
-
-        if (priority.textContent === "High" && status.textContent !== "Closed") {
-            priority.classList.add("high-priority-ticket");
-        }
-
-        if (status.textContent === "Closed") {
-            ticket.classList.add("closed-ticket");
-        }
+    if (priority.textContent === "High") {
+        priority.classList.add("high-priority-ticket");
     }
 }
